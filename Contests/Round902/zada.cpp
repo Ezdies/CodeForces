f02a8b26b9b99092cfee8d425b58d4be25e5fcd0
Ezdies <bvbclubbvb@gmail.com>
2023-10-26 11:09:08 +0200

    Unfinished version of contest problem

---
+++ Contests/Round902/zada.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <algorithm>
+
+const int size = 10000;
+
+struct Resident
+{
+    int numberOfShares;
+    int costOfShare;
+};
+
+void solve(Resident *residents, const int n, const int chanekCost)
+{
+    std::sort(residents, residents + n,
+              [](const Resident &a, const Resident &b)
+              {
+                  return a.costOfShare < b.costOfShare;
+              });
+
+    int totalShares = 0;
+    int cost = 0;
+
+    for (int i = 0; i < n;)
+    {
+        if (totalShares < n)
+        {
+            cost += residents[i].costOfShare * residents[i].numberOfShares;
+            totalShares += residents[i].numberOfShares;
+            i++;
+        }
+        else
+        {
+            i++;
+        }
+    }
+    
+    std::cout << cost  << std::endl;
+}
+
+void fillInput(Resident *residents, const int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        std::cin >> residents[i].numberOfShares;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        std::cin >> residents[i].costOfShare;
+    }
+}
+
+int main()
+{
+    int t;
+    std::cin >> t;
+
+    for (int i = 0; i < t; i++)
+    {
+        int n, chanekCost;
+        std::cin >> n >> chanekCost;
+
+        Resident residents[size];
+        fillInput(residents, n);
+        solve(residents, n, chanekCost);
+    }
+
+    return 0;
+}
